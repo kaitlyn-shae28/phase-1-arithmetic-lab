b3e92550437563225a763eea6ebfe9e6dbf9b966
Kaitlyn Williamson <kaitlyn.shae28@gmail.com>
2023-08-29 20:00:45 -0400

    y

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 2
+const num2 = 31
 
-// Write your code here
+var multiply = num1*num2
+
+var random = Math.floor(Math.random() + 1);
+
+const num3 = 8
+const num4 = 2
+
+var mod = num3/num4
+
+var max = Math.max(5, 10, 15, 20)
